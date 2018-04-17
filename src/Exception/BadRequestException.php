f124250fe3781f83f38684892bf2571ed2f9811d
Domingo Oropeza <dioh_@hotmail.com>
2018-04-17 00:31:21 -0400

    feat(errors): add exception handlers

---
+++ src/Exception/BadRequestException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by Domingo Oropeza for cryptocompare
+ * Date: 17/04/2018
+ * Time: 12:28 AM
+ */
+
+namespace App\Exception;
+
+
+class BadRequestException extends ClientException
+{
+
+}
---
+++ src/Exception/ClientException.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * Created by Domingo Oropeza for cryptocompare
+ * Date: 17/04/2018
+ * Time: 12:26 AM
+ */
+
+namespace App\Exception;
+
+
+class ClientException extends \Exception
+{
+    /**
+     * Error.
+     *
+     * @var mixed
+     */
+    protected $error;
+    /**
+     * HttpCode.
+     *
+     * @var int
+     */
+    protected $httpCode;
+    /**
+     * Message.
+     *
+     * @var string.
+     */
+    protected $message;
+    /**
+     * Request.
+     *
+     * @var Request
+     */
+    protected $request;
+    /**
+     * Response.
+     *
+     * @var Response
+     */
+    protected $response;
+    /**
+     * Constructor.
+     *
+     * @param string $message  Exception message.
+     * @param string $error  Exception error.
+     * @param string $httpCode Http error code.
+     * @param mixed  $response The response.
+     * @param mixed  $request  The request.
+     */
+    public function __construct($message, $error = null, $httpCode = null, $response = null, $request = null)
+    {
+        parent::__construct($message);
+        $this->message = $message;
+        $this->error = $error;
+        $this->httpCode = $httpCode;
+        $this->response = $response;
+        $this->request = $request;
+    }
+    /**
+     * Get error.
+     *
+     * @return string The error string.
+     */
+    public function getError()
+    {
+        return $this->error;
+    }
+    /**
+     * Get the httpCode.
+     *
+     * @return int The http code.
+     */
+    public function getHttpCode()
+    {
+        return $this->httpCode;
+    }
+    /**
+     * Get the response.
+     *
+     * @return mixed The response object.
+     */
+    public function getResponse()
+    {
+        return $this->response;
+    }
+    /**
+     * Get the request.
+     *
+     * @return mixed The request object.
+     */
+    public function getRequest()
+    {
+        return $this->request;
+    }
+}
