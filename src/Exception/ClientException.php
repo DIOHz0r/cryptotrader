7b467afaf76a02d9862886a9b67a74447a0d82ee
Domingo Oropeza <dioh_@hotmail.com>
2018-08-25 23:11:44 -0400

    doc: update header documentation
    Signed-off-by: Domingo Oropeza <dioh_@hotmail.com>

--- src/Exception/ClientException.php
+++ src/Exception/ClientException.php
@@ -1,8 +1,20 @@
 <?php
 /**
- * Created by Domingo Oropeza for cryptocompare
- * Date: 17/04/2018
- * Time: 12:26 AM
+ * cryptotrader
+ * Copyright (C) 2018 Domingo Oropeza
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 namespace App\Exception;
