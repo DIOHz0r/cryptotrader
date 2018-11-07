7d229e5d4bbbbbf086e500316f22de2aef5bd13c
Domingo Oropeza <dioh_@hotmail.com>
2018-11-07 10:46:29 -0400

    refactor: print exception errors directly to output
    Signed-off-by: Domingo Oropeza <dioh_@hotmail.com>

--- src/HttpClient/Handler/ErrorHandler.php
+++ src/HttpClient/Handler/ErrorHandler.php
@@ -19,14 +19,9 @@
 
 namespace App\HttpClient\Handler;
 
-use App\Exception\ApiLimitExceedException;
-use App\Exception\AuthenticationRequiredException;
-use App\Exception\BadRequestException;
 use App\Exception\ConnectException;
 use App\Exception\LogicException;
-use App\Exception\NotFoundException;
 use App\Exception\RuntimeException;
-use App\Exception\TwoFactorAuthenticationRequiredException;
 use GuzzleHttp\Exception\ConnectException as GuzzleConnectException;
 use GuzzleHttp\Exception\RequestException;
 
@@ -44,7 +39,7 @@ class ErrorHandler
      *
      * @param array $options
      */
-    public function __construct(array $options = array())
+    public function __construct(array $options = [])
     {
         $this->options = $options;
     }
@@ -54,14 +49,10 @@ class ErrorHandler
      *
      * @param \Exception $e The exception.
      *
-     * @throws ApiLimitExceedException
-     * @throws AuthenticationRequiredException
-     * @throws BadRequestException
+     * @return string
      * @throws ConnectException
      * @throws LogicException
-     * @throws NotFoundException
      * @throws RuntimeException
-     * @throws TwoFactorAuthenticationRequiredException
      */
     public function onException(\Exception $e)
     {
@@ -82,59 +73,17 @@ class ErrorHandler
      *
      * @param RequestException $e The request exception.
      *
-     * @throws ApiLimitExceedException
-     * @throws AuthenticationRequiredException
-     * @throws BadRequestException
-     * @throws NotFoundException
+     * @return string
      * @throws RuntimeException
-     * @throws TwoFactorAuthenticationRequiredException
      */
     protected function onRequestException(RequestException $e)
     {
-        $request = $e->getRequest();
         $response = $e->getResponse();
         if (!$response) {
             throw new RuntimeException($e->getMessage(), $e->getCode());
         }
-        $statusCode = $response->getStatusCode();
-        $isClientError = $response->isClientError();
-        $isServerError = $response->isServerError();
-        if ($isClientError || $isServerError) {
-            $content = $response->getContent();
-            $error = $response->getError();
-            $description = $response->getErrorDescription();
-            if (400 === $statusCode) {
-                throw new BadRequestException($description, $error, $statusCode, $response, $request);
-            }
-            if (401 === $statusCode) {
-                $otp = (string)$response->getHeader('OTP-Token');
-                if ('required' === $otp) {
-                    $description = 'Two factor authentication is enabled on this account';
-                    throw new TwoFactorAuthenticationRequiredException(
-                        $description,
-                        $error,
-                        $statusCode,
-                        $response,
-                        $request
-                    );
-                }
-                throw new AuthenticationRequiredException($description, $error, $statusCode, $response, $request);
-            }
-            if (404 === $statusCode) {
-                $description = sprintf('Object or route not found: %s', $request->getPath());
-                throw new NotFoundException($description, 'not_found', $statusCode, $response, $request);
-            }
-            if (429 === $statusCode) {
-                $rateLimit = $response->getApiRateLimit();
-                $description = sprintf(
-                    'You have reached the API limit. API limit is: %s. Your remaining requests will be reset at %s.',
-                    $rateLimit['limit'],
-                    date('Y-m-d H:i:s', $rateLimit['reset'])
-                );
-                throw new ApiLimitExceedException($description, $error, $statusCode, $response, $request);
-            }
-            throw new RuntimeException($description, $error, $statusCode, $response, $request);
-        }
+
+        return 'HTTP Code: '.$response->getStatusCode()."\n".'Message: '.$response->getBody()->getContents()."\n";
     }
 
 }
